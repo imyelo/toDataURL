9bf6a7aa6291a5c1a58c6f8840187ff6924655a8
yelo <zhihuzeye@gmail.com>
2014-11-05 17:01:48 +0800

    support async, bug fixed

--- libs/toDataURL.js
+++ libs/toDataURL.js
@@ -10,7 +10,49 @@
     this[name] = definition();
   }
 })('toDataURL', function (require, exports, module) {
-  var toDataURL = function (elem, width, height) {
+
+  function clean () {
+    var i, len;
+    var args = [].slice.call(arguments, 0);
+    for (i = 0, len = args.length; i < len; i++) {
+      args[i] = null;
+    }
+  }
+
+  function once (func) {
+    var ran, result;
+    return function() {
+      if (ran) {
+        return result;
+      }
+      ran = true;
+      result = func.apply(this, arguments);
+      func = null;
+      return result;
+    };
+  }
+      
+  function isImage (value) {
+    return value && value.nodeName.toLowerCase() === 'img' || false;
+  }
+
+  function createImage (src, callback) {
+    var img = new Image();
+    var cb = once(callback);
+    img.onload = function () {
+      return cb(null, img);
+    };
+    img.onerror = function () {
+      return cb(new Error('fail to load image file'));
+    };
+    img.crossOrigin = 'anonymous';
+    img.src = src;
+    if (img.complete) {
+      return cb(null, img);
+    }
+  }
+
+  function convert (elem, width, height) {
     var canvas, context, result;
     width = width || elem.width;
     height = height || elem.height;
@@ -20,18 +62,76 @@
     canvas.height = height;
     context.drawImage(elem, 0, 0, width, height);
     result = canvas.toDataURL();
-    canvas = null;
-    context = null;
+    clean(canvas, context);
     return result;
+  }
+
+  function purify (str) {
+    return str.replace(/^data:image\/(png|jpg);base64,/, '');
+  }
+
+  /**
+   * toDataURL
+   * @param  {Image|String} image
+   * @param  {Object} options
+   * @return {null|String}
+   *
+   * Options:
+   *   
+   *   - `width` {Number=0}
+   *   - `height` {Number=0}
+   *   - `purify` {Boolean=false}
+   *   - `callback` {Function}
+   *
+   * Callback:
+   *
+   *   - `err`
+   *   - `data`
+   * 
+   */
+  var toDataURL = function (image, options) {
+    var src = '';
+    var async, callback;
+
+    options = options || {};
+
+    async = typeof options.callback === 'function';
+
+    callback = async ? options.callback : function (err, data) {
+      if (err) {
+        throw err;
+      }
+      return data;
+    };
+
+    function _convert (err, img) {
+      if (err) {
+        return callback(err);
+      }
+      var result;
+      img.crossOrigin = 'anonymous';
+      result = convert(img, options.width, options.height);
+      return callback(null, (options.purify ? purify(result) : result));
+    }
+
+    if (isImage(image)) {
+      src = image.src || '';
+    } else if (typeof image === 'string') {
+      src = image;
+    } else {
+      return callback(new Error('image should be an Image Object or a string'));
+    }
+
+    return async ? createImage(src, _convert) : _convert(null, image);
   };
 
   if (typeof jQuery !== 'undefined') {
     (function ($) {
       $.fn.extend({
-        toDataURL: function (width, height) {
+        toDataURL: function (options) {
           var elem = this[0];
           if (elem) {
-            return toDataURL(elem, width, height);
+            return toDataURL(elem, options);
           }
         }
       });
